0f63410e55038645e0754fc1d89de0f28a486b08
Rohit Kumar <rajr54827@gmail.com>
2022-05-05 15:58:59 +0530

    language-btn function svaing

--- pop.js
+++ pop.js
@@ -19,3 +19,38 @@ bodyr.addEventListener("click",function(e){
   }
    console.log("hi")
  })
+
+
+
+
+
+  
+ var bodd=document.querySelector("body")
+ var expdia=document.querySelector("#logo");
+ 
+ 
+ 
+ expdia.addEventListener("click",function(){
+   window.location.href="index.html";
+ })
+ 
+ var triger_lang=document.querySelector(".for-bg")
+ triger_lang.addEventListener("click",function(e){
+   if(e.target!==triger_lang)return;
+   else{
+   triger_lang.classList.remove("triger-bg")
+   bodd.style.position="unset"
+ 
+   }
+ })
+ var lang_btn=document.querySelector("#language-btn");
+ 
+ lang_btn.addEventListener("click",function(e){
+ 
+ triger_lang.classList.add("triger-bg")
+ 
+ bodd.style.position="fixed"
+ 
+ 
+ })
+ 
